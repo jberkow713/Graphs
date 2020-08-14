204a777c6447db2526a8ee184e993f51a0b177fc
jesse berkowitz <jberkow@brandeis.edu>
2020-08-14 16:36:37 -0400

    removed queue and stack

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -19,46 +19,6 @@ class Player:
         else:
             print("You cannot move in that direction.")
 
-class Queue:
-
-    def __init__(self):
-        self.storage = []
-        self.size = 0
-
-    def enqueue(self, value):
-        self.size += 1
-        self.storage.append(value)
-
-    def dequeue(self):
-        if len(self.storage) > 0:
-            self.size -= 1
-            return self.storage.pop(0)
-        else:
-            return None
-
-    def length(self):
-        return self.size 
-
-class Stack:
-
-    def __init__(self):
-        self.storage = []
-        self.size = 0
-
-    def push(self, value):
-        self.size += 1
-        self.storage.append(value)
-
-    def pop(self):
-        if len(self.storage) > 0:
-            self.size -= 1
-            return self.storage.pop()
-
-        else:
-            return None
-
-    def length(self):
-        return self.size 
 
 
 
